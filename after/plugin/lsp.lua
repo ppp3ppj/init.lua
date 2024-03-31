512680efe5b80a472d517356cd2a74ff8a4aa115
ppp <ppp.3ppj@gmail.com>
2024-03-31 22:17:12 +0700

    add copilot and htmx lsp confix.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -57,3 +57,74 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
+
+--[[
+-- LSP Configuration for fix HTMX, TailwindCSS, Templ, HTML
+--]]--
+
+local custom_format = function()
+    if vim.bo.filetype == "templ" then
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+        vim.fn.jobstart(cmd, {
+            on_exit = function()
+                -- Reload the buffer only if it's still the current buffer
+                if vim.api.nvim_get_current_buf() == bufnr then
+                    vim.cmd('e!')
+                end
+            end,
+        })
+    else
+        vim.lsp.buf.format()
+    end
+end
+
+local on_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    -- other configuration options
+    vim.keymap.set("n", "<leader>lf", custom_format, opts)
+end
+
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local lspconfig = require("lspconfig")
+
+
+vim.filetype.add({ extension = { templ = "templ" } })
+
+lspconfig.tailwindcss.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+  init_options = { userLanguages = { templ = "html" } },
+})
+
+lspconfig.templ.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.html.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "templ" },
+})
+
+lspconfig.htmx.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "templ" },
+})
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "templ" },
+  sync_install = false,
+  auto_install = true,
+  ignore_install = { "javascript" },
+  highlight = {
+    enable = true,
+  },
+}
+
+
