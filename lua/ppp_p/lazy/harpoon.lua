3e447854f5aa2b0a8a70423ee7d4499c67d25da7
ppp <ppp.3ppj@gmail.com>
2024-04-08 22:00:51 +0700

    add harpoon config.

--- lua/ppp_p/lazy/harpoon.lua
+++ lua/ppp_p/lazy/harpoon.lua
@@ -6,13 +6,16 @@ return {
 	},
 	config = true,
 	keys = {
-		{ "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+		{ "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
 		{ "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
 		{ "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
-		{ "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+		{ "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+        { "<C-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Go to harpoon mark 1" },
+        { "<C-t>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Go to harpoon mark 2" },
+        { "<C-n>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Go to harpoon mark 3" },
+        { "<C-s>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Go to harpoon mark 4" },
 	},
 }
-
 --local mark = require("harpoon.mark")
 --local ui = require("harpoon.ui")
 
