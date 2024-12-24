22dabcb1a0b1673082fb6cf477baa62125ba0836
ppp3ppj <ppp.3ppj@gmail.com>
2024-12-24 14:58:47 +0700

    add: keymap for go error-handling patterns make it quicker and more efficient to write prepetitive boilerplate code

--- lua/ppp_p/remap.lua
+++ lua/ppp_p/remap.lua
@@ -45,6 +45,32 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/ppp_p/packer.lua<CR>");
 
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
+
+
+-- Golang keymap error
+vim.keymap.set(
+    "n",
+    "<leader>ea",
+    "oassert.NoError(err, \"\")<Esc>F\";a"
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>ef",
+    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>el",
+    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
+)
+
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
