882e2262472d159df65068d61c467ecb5c72e498
ppp <ppp.3ppj@gmail.com>
2025-03-05 20:18:13 +0700

    refactor: ppp fugitive group name

--- lua/ppp_p/lazy/fugitive.lua
+++ lua/ppp_p/lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local ppp_fugitive = vim.api.nvim_create_augroup("ppp_fugitive", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+            group = ppp_fugitive,
             pattern = "*",
             callback = function()
                 if vim.bo.ft ~= "fugitive" then
