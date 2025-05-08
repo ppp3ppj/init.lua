c75f9b59b1b2513ce6da45c78cdfcbf74450b377
ppp <ppp.3ppj@gmail.com>
2025-05-09 00:02:39 +0700

    fix: lsp mason error enabled?

--- lua/ppp_p/lazy/lsp.lua
+++ lua/ppp_p/lazy/lsp.lua
@@ -1,8 +1,12 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+        -- Mason is pinned to version 1 for now: https://github.com/LazyVim/LazyVim/issues/6039
+        --
+        -- "williamboman/mason.nvim",
+        -- "williamboman/mason-lspconfig.nvim",
+        { "mason-org/mason.nvim", version = "^1.0.0" },
+        { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
