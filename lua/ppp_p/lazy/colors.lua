b71ca6cd9cb5896e746265262526fbf9fa1ed5ae
ppp3ppj <ppp.3ppj@gmail.com>
2025-06-05 19:15:25 +0700

    add: default theme use gruber darker theme

--- lua/ppp_p/lazy/colors.lua
+++ lua/ppp_p/lazy/colors.lua
@@ -1,12 +1,41 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine-moon"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
+    {
+        "blazkowolf/gruber-darker.nvim",
+        lazy = false,
+        opts = {
+            bold = false,
+            invert = {
+                signs = false,
+                tabline = false,
+                visual = false,
+            },
+            italic = {
+                strings = false,
+                comments = false,
+                operators = false,
+                folds = true,
+            },
+            undercurl = true,
+            underline = true,
+        },
+        config = function()
+            -- require("gruber-darker").setup()
+            -- vim.api.nvim_set_hl(0, "@property", { fg = "#ffffff" })
+            -- vim.cmd([[colorscheme gruber-darker]])
+            vim.cmd.colorscheme("gruber-darker")
+
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        end,
+    },
     {
         "erikbackman/brightburn.vim",
     },
@@ -15,7 +44,7 @@ return {
         lazy = false,
         opts = {},
         config = function()
-            ColorMyPencils()
+            -- ColorMyPencils()
         end
     },
     {
@@ -40,7 +69,7 @@ return {
                 invert_tabline = false,
                 invert_intend_guides = false,
                 inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
+                contrast = "",  -- can be "hard", "soft" or empty string
                 palette_overrides = {},
                 overrides = {},
                 dim_inactive = false,
@@ -54,8 +83,8 @@ return {
             require("tokyonight").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
+                style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true,     -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
                     -- Style to be applied to different syntax groups
@@ -64,7 +93,7 @@ return {
                     keywords = { italic = false },
                     -- Background styles. Can be "dark", "transparent" or "normal"
                     sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
+                    floats = "dark",   -- style for floating windows
                 },
             })
         end
@@ -80,7 +109,7 @@ return {
                     italic = false,
                 },
             })
-            ColorMyPencils()
+            -- ColorMyPencils()
         end
     },
 
