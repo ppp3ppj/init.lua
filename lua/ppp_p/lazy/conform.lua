5c312c4dcc5c9e8b5a1b8cc9c887f1f355e456d5
ppp3ppj <ppp.3ppj@gmail.com>
2025-06-25 20:54:08 +0700

    add: conform for format code

---
+++ lua/ppp_p/lazy/conform.lua
@@ -0,0 +1,17 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "gofmt" },
+                elixir = { "mix" },
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+                typescript = { "prettierd" },
+                json = { "prettierd" },
+                css = { "prettierd" },
+            }
+        })
+    end
+}
