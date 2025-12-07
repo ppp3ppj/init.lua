c22d3979687fe26df27d560c326f4f426881fbcf
ppp3ppj <ppp.3ppj@gmail.com>
2025-12-07 14:34:36 +0700

    Update nvim and fix luasnip version

--- lua/ppp_p/lazy/lsp.lua
+++ lua/ppp_p/lazy/lsp.lua
@@ -12,7 +12,13 @@ return {
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
+        {
+            "L3MON4D3/LuaSnip",
+            -- follow latest release.
+            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp"
+        },
 		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
 		"stevearc/conform.nvim",
